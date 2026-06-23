e71e07e61bd6eda7e8e78b34a2356eb7409c915a
Rishabh Mishra <165871493+Rishabh4641U@users.noreply.github.com>
2026-06-23 03:37:21 +0000

    Add SHA256 converter and hash function

--- python-project/sha256-convert.py
+++ python-project/sha256-convert.py
@@ -1,2 +1,15 @@
 import hashlib
 import os
+
+password = input("Please enter your password: ")
+
+salt = os.urandom(16)
+hashed = hashlib.pbkdf2_hmac(
+    'sha256',
+    password.encode('utf-8'),
+    salt,
+    100000
+)
+
+print("this is your hash 🔓🔓🔓👇👇👇")
+print(hashed.hex())
